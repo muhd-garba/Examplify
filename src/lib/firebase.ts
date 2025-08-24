4774398a12a83fa9a1c595327cc4891b18f8cfc4
Your Name <muhdgarba1038@gmail.com>
2025-08-24 16:33:32 +0000

    is still not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,7 +20,13 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app;
+if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApp();
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
